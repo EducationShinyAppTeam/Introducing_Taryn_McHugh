6ad28282fdf11f2b0afbb02b031baa1fa8319751
tarynmch <tarynymchugh@gmail.com>
2023-05-30 15:52:03 -0400

    Post Meeting Edits
    Deleted unnecessary empty lines and comments. Dried timeline code. Shortened alt text. Fit what I could onto one line.

--- app.R
+++ app.R
@@ -4,22 +4,21 @@ library(shinydashboard)
 library(shinyBS)
 library(shinyWidgets)
 library(boastUtils)
-library(ggplot2) # data visualization
+library(ggplot2) 
 
 # Load additional dependencies and setup functions
 # source("global.R")
 
-## Load Dataset
+## Datasets ----
 pets <- read.csv("pets.csv")
 
 # Define UI for App ----
 ui <- list(
-  ## Create the app page ----
+  ## App page ----
   dashboardPage(
     skin = "blue",
-    ### Create the app header ----
     dashboardHeader(
-      title = "Introducing Taryn McHugh", # You may use a shortened form of the title here
+      title = "Introducing Taryn McHugh",
       titleWidth = 250,
       tags$li(class = "dropdown", actionLink("info", icon("info"))),
       tags$li(
@@ -33,7 +32,7 @@ ui <- list(
         )
       )
     ),
-    ### Create the sidebar/left navigation menu ----
+    ### Navigation menu ----
     dashboardSidebar(
       width = 250,
       sidebarMenu(
@@ -51,11 +50,11 @@ ui <- list(
     ### Create the content ----
     dashboardBody(
       tabItems(
-        #### Set up the Overview Page ----
+        #### Overview Page ----
         tabItem(
           tabName = "overview",
           withMathJax(),
-          h1("Introduction to Taryn McHugh"), # This should be the full name.
+          h1("Introduction to Taryn McHugh"), 
           p("Use this application to get to  know more about Taryn McHugh!"),
           h2("Instructions"),
           tags$ol(
@@ -63,17 +62,16 @@ ui <- list(
             tags$li("See specific images that descirbe events that happened to me
                     in the Explore Tab")
           ),
-          ##### Go Button--location will depend on your goals----
+          ##### Overview to Prereq Button
           div(
             style = "text-align: center;",
             bsButton(
-              inputId = "go",
+              inputId = "overviewPrereq",
               label = "Prerequisites",
               size = "large",
-              icon = icon("book"),
+              icon = icon("book")
             )
           ),
-          ##### Create two lines of space
           br(),
           br(),
           h2("Acknowledgements"),
@@ -83,14 +81,13 @@ ui <- list(
             br(),
             "Cite this app as:",
             br(),
-            p("McHugh, T. (2023). Introducing Taryn McHugh. [R Shiny app]. 
-              Available https://psu-eberly.shinyapps.io/Introducing_Taryn_McHugh"),
+            citeApp(),
             br(),
             br(),
-            div(class = "updated", "Last Update: 05/26/2023 by T.M.")
+            div(class = "updated", "Last Update: 05/30/2023 by T.M.")
           )
         ),
-        #### Set up the Prerequisites Page ----
+        #### Prerequisites Page ----
         tabItem(
           tabName = "prerequisites",
           withMathJax(),
@@ -101,73 +98,68 @@ ui <- list(
             collapsible = TRUE,
             collapsed = FALSE,
             width = "100%",
-            p("Sports: I have always tried to participate in sports to stay active,
+            p("I have always tried to participate in sports to stay active,
           I usually wasn't any good though. When I was younger, I was on my 
           local softball team for two years, and I picked it back up for two 
           years in middle school where I played second and shortstop. In high 
           school, I was on my high schools tennis team from sophomore to senior 
           year."),
-          p("Travel: One of my goals is to travel as much as I can while I am still 
+          p("One of my goals is to travel as much as I can while I am still 
           relatively young. One of my dream destinations is Europe, potentially 
           Italy or Greece. As of now, I have been to/through all the states on 
           the east coast and some western states like Hawaii, California, Alaska, 
           and a few more. This summer I am super excited because I will be 
           going to Costa Rica by myself. I am super excited for the independency
           and experiencing another culture."),
-          p("Extra Curriculars: In high shcool I was heavily involved in extra 
+          p("In high shcool I was heavily involved in extra 
           curriculars. I was on my schools Unified Sports Teams (track/field and
           bocce), Debate Team, Tennis Team, Exec Council, and Mini THON. In college, 
-          I joined a sorority, Special Olympics, and THON."),
+          I joined a sorority, Special Olympics, and THON.")
           ),
-          
           box(
             title = strong("Home"),
             status = "primary",
             collapsible = TRUE,
             collapsed = FALSE,
             width = "100%",
-          p("Home: When I was younger I lived in the south for the mostpart.
+          p("When I was younger I lived in the south for the mostpart.
           Until I was in second grade, I lived in Loganville, GA. For third 
           grade I lived in Gadsten, AL. Currently I reside in Bucks County, PA."),
-          p("Family: I have two older sisters. My eldest sisters name is Laura,
+          p("I have two older sisters. My eldest sisters name is Laura,
           she is 27 and lives in Leesburg, VA with her fiance. My other sisters
           name is Paige, she is 22 and is graduing from SJU in Philly this
           semester."),
-          p("Pets: I have had various pets throughout my life including dogs, cats,
+          p("I have had various pets throughout my life including dogs, cats,
           turtles, fish, and bearded dragons. Currently, I have two dogs named 
           Ellie and Mariposa. Ellie is a black terrier mix and Mariposa is a white
-          Pekingese mix."),
+          Pekingese mix. Below is a pie chart of the types of pets I have had and
+          how many."),
           plotOutput(
-            outputId = "numSpeciesPlot",
-            width = "100%",
+            outputId = "numTypePlot",
+            width = "100%"
             )
-          ),
+          )
         ),
-        #### Set up an Explore Page ----
+        ####  Explore Page ----
         tabItem(
           tabName = "explore",
           withMathJax(),
           h2("Visual Timeline"),
           p("Slide the bar to each age in my life to see images of key events in
             my life for that age."),
-          fluidRow(
-            column(
-              width = 12,
-              wellPanel(
-                sliderInput(
-                  inputId = "age",
-                  label = "Age",
-                  min = 9,
-                  max = 18,
-                  step = 3,
-                  value = 3
-                  )
-                ),
-              uiOutput('event'),
+          wellPanel(
+            sliderInput(
+              inputId = "age",
+              label = "Age",
+              min = 9,
+              max = 18,
+              step = 3,
+              value = 9
             )
-          )
+          ),
+          uiOutput('event')
         ),
-        #### Set up the References Page ----
+        #### References Page ----
         tabItem(
           tabName = "references",
           withMathJax(),
@@ -208,7 +200,6 @@ ui <- list(
             "Wickham H (2016). ggplot2: Elegant Graphics for Data Analysis. 
             Springer-Verlag New York. ISBN 978-3-319-24277-4. Avaliable from 
             https://ggplot2.tidyverse.org."
-            
           ),
           br(),
           br(),
@@ -222,125 +213,90 @@ ui <- list(
 
 # Define server logic ----
 server <- function(input, output, session) {
-  
-  ## Species Plot Rendering
-  output$numSpeciesPlot <-renderPlot({
-    ggplot(data = pets,
-           mapping = aes(x = "", 
-                         y = Number, 
-                         fill=Species)) +
-      geom_bar(stat = "identity", 
-               width = 1) +
+  ## Pet Plot Rendering ----
+  output$numTypePlot <- renderPlot({
+    ggplot(
+      data = pets,
+      mapping = aes(x = "", y = Number, fill = Type)
+    ) +
+      geom_bar(stat = "identity", width = 1) +
       coord_polar("y", start = 0) +
-      geom_text(size = 7,
-                aes(label = Number), 
-                position = position_stack(vjust = 0.5)) +
-      theme(axis.text = element_blank(), 
-            legend.text = element_text(size = 17),
-            legend.title = element_text(size = 17)) + 
-      labs(x = NULL, 
-           y = NULL,
-           fill = 'Species')
-      },
-    alt = "Pie chart of the amount of pets I have had by species."
-    )
+      geom_text(
+        size = 7,
+        aes(label = Number), 
+        position = position_stack(vjust = 0.5)
+      ) +
+      theme(
+        axis.text = element_blank(), 
+        legend.text = element_text(size = 17),
+        legend.title = element_text(size = 17)
+      ) + 
+      labs(
+        x = NULL, 
+        y = NULL,
+        fill = 'Type',
+        title = "Type of Pets by Number")
+  },
+  alt = "Pie chart of the amount of pets I have had by type."
+  )
   
   ## Explore Image ----
-  output$event <- renderUI({
-    if(input$age == "9"){
-      list(      
-        fluidRow(
-          column(
-            width = 4,
-            offset = 1,
-            img(height = "100%",
-                width = "100%",
-              src = "age9.jpg",
-              alt = "Image of a sunset in the neighborhood.")
-              ),
-          column(
-            width = 6,
-            offset = 1,
-            p("When I was 9, I moved to Pennslyvania. This is a picture of a sunset 
-        outside my house. ")
-          )
-        )
-      )
-
-    } else if(input$age == "12"){
-      list(
-        fluidRow(
-          column(
-            width = 4,
-            offset = 0,
-            img(height = "100%",
-                width = "100%",
-              src = "age12.jpg",
-              alt = "Imagine of a wooden sign that says Welcome to Alaska and 
-              the Gateway to the Klondike.")
-          ),
-          column(
-            width = 7,
-            offset = 1,
-            p("When I was 12 was when I travelled across the country for the 
+  observeEvent(
+    eventExpr = input$age,
+    handlerExpr = {
+      if (input$age == "9") {
+        imgSrc <- "age9.jpg"
+        imgAlt <- "Sunset in a neighborhood"
+        paraText <- "When I was 9, I moved to Pennslyvania. This is a picture of 
+        a sunset outside my house. "
+      } else if (input$age == "12") {
+        imgSrc <- "age12.jpg"
+        imgAlt <- "Wooden sign that says, 'Welcome to Alaska and the Gateway to 
+        the Klondike.'"
+        paraText <- "When I was 12 was when I travelled across the country for the 
               first time. We went on a cruise to visit various locaitons in Alaska 
-              and Canada.")
-           )
-        )
-     )
-    
-    } else if(input$age == "15") {
-      list(
-        fluidRow(
-          column(
-            width = 3,
-            offset = 1,
-            img(height = "100%",
-                width = "100%",
-                src = "age15.jpg",
-                alt = "A collage of photos of Taryn when she was 15. It includes her 
-          and her 2 frineds, her and she sister in a agraduation gown, and her 
-          and her freinds in tennis uniforms.")
-          ),
-          column(
-            width = 7,
-            offset = 1,
-            p("Here are some pictures from when I was 15. The left image is a picture 
-        of my friends and I when we went to Ocean City, NJ together. My sister 
-        also graduated high school (top right). Sophomore year was when I was 15,
-        so that I when I joined the tennis team. It is a picture of me and my friends
-        at a match (lower right).")
-          )
-       )
-    )
-        
-    } else {
-      list(
-        fluidRow(
-          column(
-            width = 3,
-            offset = 1,
-            img(height = "100%",
-                width = "100%",
-                src = "age18.jpg",
-                alt = "A collage of photos of Taryn when she was 18. There is an 
-                image of her at her high school graudation, her posing for her 
-                first day of college, and a botanical garden from when she went 
-                to Hawaii.")
-          ),
-          column(
-            width = 7,
-            offset = 1,
-            p("A lot of exciting things happened when I was 18. I graduated high 
+              and Canada."
+      } else if (input$age == "15") {
+        imgSrc <- "age15.jpg"
+        imgAlt <- "A collage of photos of Taryn. Her and her friends posing, 
+                ,her and her sister in a graduation gown, and her and her friends 
+                in tennis uniforms."
+        paraText <- "Here are some pictures from when I was 15. The left image is 
+        a picture of my friends and I when we went to Ocean City, NJ together.
+        My sister also graduated high school (top right). Sophomore year was when
+        I was 15, so that I when I joined the tennis team. It is a picture of me
+        and my friends at a match (lower right)."
+      } else {
+        imgSrc <- "age18.jpg"
+        imgAlt <- "A collage of photos of Taryn. Her high school graduation, her 
+                posing for her first day of college, and a botanical garden from 
+                Hawaii."
+        paraText <- "A lot of exciting things happened when I was 18. I graduated high 
               school (left). Over the summer, I went to Hawaii for the right time 
-              (upper right). Then started my first day of college at PSU (bottom right).")
+              (upper right). Then started my first day of college at PSU (bottom right)."
+      }
+      output$event <- renderUI({
+        list(
+          fluidRow(
+            column(
+              width = 3,
+              offset = 1,
+              img(
+                width = "100%",
+                src = imgSrc,
+                alt = imgAlt
+              )
+            ),
+            column(
+              width = 7,
+              offset = 1,
+              p(paraText)
+            )
           )
         )
-      )
-          }
-  })
-  
-
+      })
+    }
+  )
   ## Set up Info button ----
   observeEvent(
     eventExpr = input$info,
@@ -353,9 +309,10 @@ server <- function(input, output, session) {
       )
     }
   )
+  
   ## Overview to  Button----
   observeEvent(
-    eventExpr = input$go,
+    eventExpr = input$overviewPrereq,
     handlerExpr = {
       updateTabItems(
         session = session,
@@ -365,7 +322,6 @@ server <- function(input, output, session) {
     }
   )
 }
-
 
 # Boast App Call ----
 boastUtils::boastApp(ui = ui, server = server)
